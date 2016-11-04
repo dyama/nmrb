b53ecad98dfb4ff443a91234937abc164312478d
dyama <dyama@member.fsf.org>
2016-11-04 15:10:04 +0900

    Add test.

---
+++ nmrb_unittest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using nmrb;
+
+namespace nmrb_unittest
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void test1()
+    {
+      State mrb = new State();
+      mrb.Dispose();
+      mrb = null;
+    }
+
+    [TestMethod]
+    public void test2()
+    {
+      using (var mrb = new State()) {
+        Assert.AreEqual(new FixnumValue(123).ToInt32(), 123);
+        Assert.AreEqual(new FloatValue(mrb, 3.14).ToDouble(), 3.14);
+        Assert.AreEqual(new TrueValue().ToBoolean(), true);
+        Assert.AreEqual(new FalseValue().ToBoolean(), false);
+        Assert.AreEqual(new StringValue(mrb, "foobarbaz").ToString(mrb), "foobarbaz");
+        Assert.AreEqual(new SymbolValue(mrb, "hogefuga").ToString(mrb), "hogefuga");
+      }
+    }
+
+    [TestMethod]
+    public void test3()
+    {
+      using (var mrb = new State()) {
+        dynamic r = mrb["123"];
+        //mrb.Do("true");
+        // // Assert.AreEqual(t, nmrb.ValueType.TRUEVAL);
+        //Assert.AreEqual(mrb.Do("false").ToBoolean(), false);
+      }
+    }
+
+    [TestMethod]
+    [Ignore]
+    public void unicodecheck()
+    {
+      //Assert.AreEqual(new StringValue(mrb, "こんにちは").ToString(mrb), "こんにちは");
+    }
+
+  }
+}
