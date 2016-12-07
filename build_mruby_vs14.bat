4f2c0bbb1964394c554b7570f3fb9cc679d3d06b
dyama <dyama@member.fsf.org>
2016-12-07 10:45:19 +0900

    Tune build options.

--- build_mruby_vs14.bat
+++ build_mruby_vs14.bat
@@ -1,9 +1,11 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 
 REM for git.exe and bison.exe
 set "PATH=E:\Git\bin;%PATH%"
 
 cd mruby
+del vc140.pdb
 ruby minirake clean
 ruby minirake default
 cd ..
+pause
--- build_mruby_vs15.bat
+++ build_mruby_vs15.bat
@@ -4,6 +4,8 @@ REM for git.exe and bison.exe
 set "PATH=E:\Git\bin;%PATH%"
 
 cd mruby
+del vc140.pdb
 ruby minirake clean
 ruby minirake default
 cd ..
+pause
